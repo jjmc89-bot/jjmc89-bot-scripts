ac038b1ddf58e78e9eda0f957c3475c77a6cd85f
jc86035 <jc86035@openmailbox.org>
2017-07-12 11:49:33 +0800

    [bsicons_replacer] Update prefix_map
    - Add 'e': {{BS1e}}, {{FLe}}, {{FL1e}}, {{JBS1u}}, {{ZCn}}, {{ZC1n}}, {{ŽČn}}, {{ŽČ1n}}
    - Add 'u': {{BS1u}}, {{FLm}}, {{FL1m}}, {{ZCm}}, {{ZC1m}}, {{ŽČm}}, {{ŽČ1m}}
    - Add 'ue': {{BS1ue}}, {{FLme}}, {{FL1me}}

--- multi/bsicons_replacer.py
+++ multi/bsicons_replacer.py
@@ -189,12 +189,32 @@ class BSiconsReplacer(SingleSiteBot, ExistingPageBot, NoRedirectPageBot):
         self.prefix_map = {
             'e': self.get_template_titles([
                 pywikibot.Page(self.site, 'Template:BSe'),
-                pywikibot.Page(self.site, 'Template:JBSu')
+                pywikibot.Page(self.site, 'Template:BS1e'),
+                pywikibot.Page(self.site, 'Template:JBSu'),
+                pywikibot.Page(self.site, 'Template:JBS1u'),
+                pywikibot.Page(self.site, 'Template:ZCn'),
+                pywikibot.Page(self.site, 'Template:ZC1n'),
+                pywikibot.Page(self.site, 'Template:ŽČn'),
+                pywikibot.Page(self.site, 'Template:ŽČ1n'),
+                pywikibot.Page(self.site, 'Template:FLe'),
+                pywikibot.Page(self.site, 'Template:FL1e')
             ]),
             'u': self.get_template_titles([
-                pywikibot.Page(self.site, 'Template:BSu')]),
+                pywikibot.Page(self.site, 'Template:BSu'),
+                pywikibot.Page(self.site, 'Template:BS1u'),
+                pywikibot.Page(self.site, 'Template:ZCm'),
+                pywikibot.Page(self.site, 'Template:ZC1m'),
+                pywikibot.Page(self.site, 'Template:ŽČm'),
+                pywikibot.Page(self.site, 'Template:ŽČ1m'),
+                pywikibot.Page(self.site, 'Template:FLm'),
+                pywikibot.Page(self.site, 'Template:FL1m')
+            ]),
             'ue': self.get_template_titles([
-                pywikibot.Page(self.site, 'Template:BSue')]),
+                pywikibot.Page(self.site, 'Template:BSue'),
+                pywikibot.Page(self.site, 'Template:BS1ue'),
+                pywikibot.Page(self.site, 'Template:FLme'),
+                pywikibot.Page(self.site, 'Template:FL1me')
+            ]),
         }
 
     def get_template_titles(self, templates):
