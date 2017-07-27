780e8de2e31ca06168976acba7de5d056293d4f7
JJMC89 <JJMC89@gmail.com>
2017-07-26 19:50:27 -0700

    update description

--- enwiki/inactive_admins.py
+++ enwiki/inactive_admins.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-This script reports inactive admins.
+This script reports and notifies inactive admins.
 
 
 The following parameters are required:
