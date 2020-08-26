jjmc89-bot/jjmc89-bot-scripts

commit 5261f20ffc4f2721e89b46eba722deeea1ed06e5

Misc code cleanup
- Pass site to GeneratorFactory
- Pass site to subclasses of SingleSiteBot
- Drop pagegenerators import
- Use str.format instead of %

enwiki/editnotice_deployer.py
@@ -3,13 +3,15 @@
 """
 This script deploys editnotices.
 
+The following parameters are required:
+
+-editnotice_template Title of the editnotice template
+
 The following parameters are supported:
 
 -always           Don't prompt to save changes.
 
--editnoticeTemplate Title of the editnotice template
-
--subject_Only     Restrict to subject pages
+-subject_only     Restrict to subject pages
 
 -talk_only        Restrict to talk pages
 
@@ -21,15 +23,34 @@ The following parameters are supported:
 """
 # Author : JJMC89
 # License: MIT
-import sys
 import mwparserfromhell
 import pywikibot
-from pywikibot import pagegenerators
 from pywikibot.bot import SingleSiteBot, CurrentPageBot
 
 docuReplacements = { # pylint: disable=invalid-name
-    '&params;': pagegenerators.parameterHelp
+    '&params;': pywikibot.pagegenerators.parameterHelp
 }
+
+
+def get_template_titles(templates):
+    """
+    Given an iterable of templates, return a set of pages.
+
+    @param templates: iterable of templates (L{pywikibot.Page})
+    @type templates: iterable
+
+    @rtype: set
+    """
+    titles = set()
+    for template in templates:
+        if template.isRedirectPage():
+            template = template.getRedirectTarget()
+        if not template.exists():
+            continue
+        titles.add(template.title(with_ns=template.namespace() != 10))
+        for tpl in template.backlinks(filter_redirects=True):
+            titles.add(tpl.title(with_ns=tpl.namespace() != 10))
+    return titles
 
 
 def validate_options(options, site):
@@ -45,27 +66,22 @@ def validate_options(options, site):
     required_keys = ['editnotice_template']
     has_keys = list()
     for key, value in options.items():
-        pywikibot.log('-%s = %s' % (key, value))
+        pywikibot.log('-{} = {}'.format(key, value))
         if key in required_keys:
             has_keys.append(key)
-        if key in ('subject_only', 'talk_only', 'to_subject', 'to_talk'):
-            pass
-        elif key == 'editnotice_template':
-            if isinstance(key, str):
-                editnotice_page = pywikibot.Page(site, 'Template:%s' % value)
-                if not editnotice_page.exists():
-                    return False
-            else:
+        if key == 'editnotice_template':
+            if not isinstance(key, str):
+                return False
+            options[key] = '{{' + value + '}}'
+            options['editnotice_page'] = pywikibot.Page(site, value, ns=10)
+            if not options['editnotice_page'].exists():
                 return False
     if sorted(has_keys) != sorted(required_keys):
         return False
-    options['editnotice_page'] = editnotice_page
-    options.pop('editnotice_template')
     return True
 
 
-def page_with_subject_page_generator( # pylint: disable=invalid-name
-        generator, return_subject_only=False):
+def page_with_subject_page_generator(generator, return_subject_only=False):
     """
     Yield pages and associated subject pages from another generator.
 
@@ -99,7 +115,7 @@ def editnotice_page_generator(generator):
     for page in generator:
         if page.exists() and not page.isRedirectPage():
             title = page.title(withSection=False)
-            editnotice_title = 'Template:Editnotices/Page/%s' % title
+            editnotice_title = 'Template:Editnotices/Page/{}'.format(title)
             editnotice_page = pywikibot.Page(page.site, editnotice_title)
             yield editnotice_page
 
@@ -116,54 +132,39 @@ class EditnoticeDeployer(SingleSiteBot, CurrentPageBot):
         @type generator: generator
         """
         self.availableOptions.update({
-            'editnotice_page': None
+            'editnotice_page': None,
+            'editnotice_template': None
         })
         self.generator = generator
         super().__init__(**kwargs)
-        self.editnotice_page_titles = self.get_template_titles(
+        self.editnotice_page_titles = get_template_titles(
             [self.getOption('editnotice_page')])
+        self.editnotice_template = self.getOption('editnotice_template')
 
-    def get_template_titles(self, templates):
-        """
-        Given an iterable of templates, return a set of titles.
-
-        @param templates: iterable of templates (L{pywikibot.Page})
-        @type templates: iterable
-
-        @rtype: set
-        """
-        titles = set()
-        for template in templates:
-            titles.add(template.title(withNamespace=False))
-            titles.add(template.title(underscore=True, withNamespace=False))
-            for tpl in template.backlinks(
-                    filterRedirects=True,
-                    namespaces=self.site.namespaces.TEMPLATE
-            ):
-                titles.add(tpl.title(withNamespace=False))
-                titles.add(tpl.title(underscore=True, withNamespace=False))
-        return titles
-
-    def check_enabled(self):
-        """Check if the task is enabled."""
+    def check_disabled(self):
+        """Check if the task is disabled. If so, quit."""
         if self._treat_counter % 6 != 0:
             return
+        if not self.site.logged_in():
+            self.site.login()
         page = pywikibot.Page(
             self.site,
-            'User:%s/shutoff/%s' % (self.site.user(), self.__class__.__name__)
+            'User:{username}/shutoff/{class_name}.json'.format(
+                username=self.site.user(),
+                class_name=self.__class__.__name__
+            )
         )
         if page.exists():
             content = page.get(force=True).strip()
             if content:
-                sys.exit('%s disabled:\n%s' %
-                         (self.__class__.__name__, content))
+                e = '{} disabled:\n{}'.format(self.__class__.__name__, content)
+                pywikibot.error(e)
+                self.quit()
 
     def treat_page(self):
         """Process one page."""
-        self.check_enabled()
-        skip = False
-        if (not self.current_page.exists() or
-                self.current_page.isRedirectPage()):
+        self.check_disabled()
+        if self.current_page.isRedirectPage():
             text = ''
         else:
             text = self.current_page.text
@@ -171,18 +172,12 @@ class EditnoticeDeployer(SingleSiteBot, CurrentPageBot):
             # Check to make sure the editnotice isn't already there.
             for tpl in wikicode.filter_templates():
                 if tpl.name.matches(self.editnotice_page_titles):
-                    skip = True
-                    break
-        text = ('%s\n{{%s}}' % (text,
-                                self.getOption('editnotice_page').title(
-                                    withNamespace=False))).strip()
-        if not skip:
-            self.put_current(
-                text,
-                summary=('Deploying editnotice: {{%s}}' % self.getOption(
-                    'editnotice_page').title(withNamespace=False)),
-                minor=False
-            )
+                    return
+        self.put_current(
+            '\n'.join((self.editnotice_template, text)),
+            summary='Deploying editnotice: ' + self.editnotice_template,
+            minor=False
+        )
 
 
 def main(*args):
@@ -203,7 +198,7 @@ def main(*args):
     site = pywikibot.Site()
     site.login()
     # Parse command line arguments
-    gen_factory = pagegenerators.GeneratorFactory()
+    gen_factory = pywikibot.pagegenerators.GeneratorFactory(site)
     for arg in local_args:
         if gen_factory.handleArg(arg):
             continue
@@ -212,7 +207,7 @@ def main(*args):
         if arg == 'editnotice_template':
             if not value:
                 value = pywikibot.input(
-                    'Please enter a value for %s' % arg,
+                    'Please enter a value for {}'.format(arg),
                     default=None
                 )
             options[arg] = value
@@ -222,31 +217,26 @@ def main(*args):
         pywikibot.error('Invalid options.')
         return False
     gen = gen_factory.getCombinedGenerator()
-
-    if gen:
-        if options['to_subject']:
-            gen = page_with_subject_page_generator(
-                gen,
-                return_subject_only=options['subject_only']
-            )
-        elif options['to_talk']:
-            gen = pagegenerators.PageWithTalkPageGenerator(
-                gen,
-                return_talk_only=options['talk_only']
-            )
-        elif options['subject_only']:
-            gen = subject_page_generator(gen)
-        elif options['talk_only']:
-            gen = talk_page_generator(gen)
-        gen = editnotice_page_generator(gen)
-        for key in ('subject_only', 'talk_only', 'to_subject', 'to_talk'):
-            options.pop(key, None)
-        gen = pagegenerators.PreloadingGenerator(gen)
-        bot = EditnoticeDeployer(gen, **options)
-        bot.run()
-        return True
-    pywikibot.bot.suggest_help(missing_generator=True)
-    return False
+    if options['to_subject']:
+        gen = page_with_subject_page_generator(
+            gen,
+            return_subject_only=options['subject_only']
+        )
+    elif options['to_talk']:
+        gen = pywikibot.pagegenerators.PageWithTalkPageGenerator(
+            gen,
+            return_talk_only=options['talk_only']
+        )
+    elif options['subject_only']:
+        gen = subject_page_generator(gen)
+    elif options['talk_only']:
+        gen = talk_page_generator(gen)
+    gen = editnotice_page_generator(gen)
+    for key in ('subject_only', 'talk_only', 'to_subject', 'to_talk'):
+        options.pop(key, None)
+    gen = pywikibot.pagegenerators.PreloadingGenerator(gen)
+    EditnoticeDeployer(gen, site=site, **options).run()
+    return True
 
 
 if __name__ == "__main__":

enwiki/category_double_redirect_fixer.py
@@ -15,13 +15,12 @@ The following parameters are supported:
 # License: MIT
 import mwparserfromhell
 import pywikibot
-from pywikibot import pagegenerators
 from pywikibot.bot import SingleSiteBot, ExistingPageBot
 from pywikibot.textlib import removeDisabledParts
 
 
 docuReplacements = { #pylint: disable=invalid-name
-    '&params;': pagegenerators.parameterHelp
+    '&params;': pywikibot.pagegenerators.parameterHelp
 }
 
 
@@ -147,7 +146,7 @@ def main(*args):
     local_args = pywikibot.handle_args(args)
     site = pywikibot.Site()
     site.login()
-    gen_factory = pagegenerators.GeneratorFactory()
+    gen_factory = pywikibot.pagegenerators.GeneratorFactory(site)
     for arg in local_args:
         if gen_factory.handleArg(arg):
             continue
@@ -163,7 +162,7 @@ def main(*args):
         else:
             options[arg] = True
     gen = gen_factory.getCombinedGenerator(preload=True)
-    CategoryDoubleRedirectFixerBot(gen, **options).run()
+    CategoryDoubleRedirectFixerBot(gen, site=site, **options).run()
 
 
 if __name__ == '__main__':

enwiki/cfdw.py
@@ -11,13 +11,12 @@ import re
 import mwparserfromhell
 from mwparserfromhell.nodes import Template, Text, Wikilink
 import pywikibot
-from pywikibot import pagegenerators
 from pywikibot.bot import ExistingPageBot, SingleSiteBot
 from pywikibot.textlib import removeDisabledParts, replaceExcept
 
 
 docuReplacements = { #pylint: disable=invalid-name
-    '&params;': pagegenerators.parameterHelp
+    '&params;': pywikibot.pagegenerators.parameterHelp
 }
 SUMMARIES = {
     'redirect': '[[WP:G8|G8]]: Redirect to deleted page {}',
@@ -347,7 +346,7 @@ def do_action(mode, **kwargs):
             old_cat=old_cat.title(as_link=True, textlink=True),
             cfd=cfd_link
         )
-        CfdBot(gen, **kwargs).run()
+        CfdBot(gen, site=cfd.site, **kwargs).run()
         # Wait for the category to be registered as empty.
         pywikibot.sleep(pywikibot.config2.put_throttle)
         if old_cat.exists() and old_cat.isEmptyCategory():
@@ -362,12 +361,12 @@ def do_action(mode, **kwargs):
                                     for cat in kwargs['new_cats'])
         else:
             new_cats = '{} categories'.format(len(kwargs['new_cats']))
-        del kwargs['redirect']
+        kwargs.pop('redirect')
         kwargs['summary'] = 'Merging {old_cat} to {new_cats} per {cfd}'.format(
             old_cat=old_cat.title(as_link=True, textlink=True),
             new_cats=new_cats, cfd=cfd_link
         )
-        CfdBot(gen, **kwargs).run()
+        CfdBot(gen, site=cfd.site, **kwargs).run()
         # Wait for the category to be registered as empty.
         pywikibot.sleep(pywikibot.config2.put_throttle)
         if (old_cat.exists() and old_cat.isEmptyCategory()
@@ -392,7 +391,7 @@ def do_action(mode, **kwargs):
             new_cat=kwargs['new_cats'][0].title(as_link=True, textlink=True),
             cfd=cfd_link
         )
-        CfdBot(gen, **kwargs).run()
+        CfdBot(gen, site=cfd.site, **kwargs).run()
     elif mode == 'retain':
         kwargs['summary'] = '{cfd} closed as {result}'.format(
             cfd=cfd_link,
@@ -589,7 +588,7 @@ def main(*args):
     local_args = pywikibot.handle_args(args)
     site = pywikibot.Site()
     site.login()
-    gen_factory = pagegenerators.GeneratorFactory()
+    gen_factory = pywikibot.pagegenerators.GeneratorFactory(site)
     for arg in local_args:
         gen_factory.handleArg(arg)
     for key, value in TPL.items():

enwiki/draftification_report.py
@@ -23,7 +23,6 @@ from datetime import date, timedelta
 import re
 from dateutil.parser import parse as parse_date
 import pywikibot
-from pywikibot import pagegenerators
 
 BOT_START_END = re.compile(
     r'^(.*?<!--\s*bot start\s*-->).*?(<!--\s*bot end\s*-->.*)$',
@@ -88,7 +87,7 @@ def get_xfds(pages):
         else:
             prefix = 'Miscellany for deletion/'
         prefix += page.title()
-        gen = pagegenerators.PrefixingPageGenerator(
+        gen = pywikibot.pagegenerators.PrefixingPageGenerator(
             prefix, namespace=page.site.namespaces.PROJECT, site=page.site)
         xfds = xfds.union([xfd_page.title(asLink=True) for xfd_page in gen])
     return xfds

enwiki/inactive_admins.py
@@ -406,7 +406,7 @@ def main(*args):
         if arg in ('config', 'max_attempts'):
             if not value:
                 value = pywikibot.input(
-                    'Please enter a value for %s' % arg,
+                    'Please enter a value for {}'.format(arg),
                     default=None
                 )
             options[arg] = value

enwiki/inactive_interface_admins.py
@@ -145,7 +145,7 @@ def main(*args):
     site.login()
     users = get_inactive_users(site=site)
     if not users:
-        return True
+        return
     heading = 'Inactive interface administrators {}'.format(
         site.getcurrenttime().date())
     text = 'The following interface administrator(s) are inactive:'
@@ -156,7 +156,6 @@ def main(*args):
         site,
         "Wikipedia:Interface administrators' noticeboard"
     ).save(text=text, section='new', summary=heading, botflag=False)
-    return True
 
 
 if __name__ == "__main__":

enwiki/massmessage_list_updater.py
@@ -32,7 +32,6 @@ import json
 from operator import itemgetter
 import re
 import pywikibot
-from pywikibot import pagegenerators
 from pywikibot.bot import SingleSiteBot, ExistingPageBot, NoRedirectPageBot
 
 
@@ -145,8 +144,8 @@ def validate_options(options):
 
 class UserGroupsMassMessageListUpdater(
         SingleSiteBot,
-        ExistingPageBot,
-        NoRedirectPageBot
+        NoRedirectPageBot,
+        ExistingPageBot
 ):
     """Bot to update MassMessage lists."""
 
@@ -295,7 +294,7 @@ def main(*args):
     for arg in local_args:
         arg, _, value = arg.partition(':')
         arg = arg[1:]
-        if arg in 'config' 'end_date' 'start_date':
+        if arg in ('config', 'end_date', 'start_date'):
             if not value:
                 value = pywikibot.input(
                     'Please enter a value for {}'.format(arg),
@@ -375,8 +374,8 @@ def main(*args):
     # Generate pages and invoke the bot.
     gen = (config[key]['page'] for key in config.keys()
            if config[key]['enabled'])
-    gen = pagegenerators.PreloadingGenerator(gen)
-    UserGroupsMassMessageListUpdater(gen, **options).run()
+    gen = pywikibot.pagegenerators.PreloadingGenerator(gen)
+    UserGroupsMassMessageListUpdater(gen, site=site, **options).run()
     return True
 
 

multi/commons_potd_importer.py
@@ -15,11 +15,10 @@ The following parameters are supported:
 import mwparserfromhell
 from mwparserfromhell.nodes import Wikilink
 import pywikibot
-from pywikibot import pagegenerators
 from pywikibot.bot import MultipleSitesBot, ExistingPageBot
 
 docuReplacements = { # pylint: disable=invalid-name
-    '&params;': pagegenerators.parameterHelp
+    '&params;': pywikibot.pagegenerators.parameterHelp
 }
 
 
@@ -135,13 +134,14 @@ def main(*args):
     site = pywikibot.Site()
     site.login()
     # Parse command line arguments
-    gen_factory = pagegenerators.GeneratorFactory()
+    gen_factory = pywikibot.pagegenerators.GeneratorFactory(site)
     for arg in local_args:
         if gen_factory.handleArg(arg):
             continue
         if arg == '-always':
             options['always'] = True
-    CommonsPotdImporter(gen_factory.getCombinedGenerator(), **options).run()
+    gen = gen_factory.getCombinedGenerator()
+    CommonsPotdImporter(gen, site=site, **options).run()
 
 
 if __name__ == '__main__':

commit 4b90027b3e8a655e60af07d3a58bfd2f7846ff78

Fix RuntimeError
Regression introduced in 5261f20

enwiki/editnotice_deployer.py
@@ -75,11 +75,13 @@ def validate_options(options, site):
             if not isinstance(key, str):
                 return False
             options[key] = '{{' + value + '}}'
-            options['editnotice_page'] = pywikibot.Page(site, value, ns=10)
-            if not options['editnotice_page'].exists():
+            editnotice_page = pywikibot.Page(site, value, ns=10)
+            if not editnotice_page.exists():
                 return False
     if sorted(has_keys) != sorted(required_keys):
         return False
+    options['editnotice_page'] = editnotice_page
+    options.pop('editnotice_template')
     return True
 
 